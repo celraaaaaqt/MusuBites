0b57f0f0b63f3fe7c0f97c43b6e77b22a9f65e18
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-20 15:59:15 +0800

    final verdict

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
         menu: resolve(__dirname, "menu.html"),
-        contact: resolve(__dirname, "contact.html"),
+        contact: resolve(__dirname, "socmed.html"),
       },
     },
   },
