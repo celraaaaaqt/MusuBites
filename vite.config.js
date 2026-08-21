497bb6cf8eef69dd70e89c2c8c7158ac6822264c
Arcel Zamora <arcelzamora068@gmail.com>
2026-08-21 16:27:34 +0800

    done for today

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(import.meta.dirname, "index.html"),
-        about: resolve(import.meta.dirname, "about.html"),
-        menu: resolve(import.meta.dirname, "menu.html"),
-        socmed: resolve(import.meta.dirname, "socmed.html"),
-      },
-    },
-  },
 });
