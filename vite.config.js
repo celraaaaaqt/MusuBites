be73aca76736076f3531a56b2810f347d8fd3ff0
Arcel Zamora <arcelzamora068@gmail.com>
2026-09-12 19:14:31 +0800

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss()],
   assetsInclude: ['**/*.apk'],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        adminLogin: resolve(__dirname, 'admin/admin_login.html'),
+        dashboard: resolve(__dirname, 'admin/dashboard.html'),
+      }
+    }
+  }
 })
